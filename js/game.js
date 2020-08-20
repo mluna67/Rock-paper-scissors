6e19eedadab49c099489bab9609cec49f4b75246
mluna67 <lunamrko@gmail.com>
2020-08-20 14:05:41 -0500

    funcional

--- js/game.js
+++ js/game.js
@@ -11,9 +11,43 @@ btn_scissors.onclick = ()=>{
 btn_rock.onclick = ()=>{
     jugar("rock");
 }
+var puntaje = 0;
+var options = ["paper", "scissors", "rock"];
 
 function jugar(election){
     document.getElementById("step-1").style.display = "none";
     document.getElementById("step-2").style.display = "block";
-    console.log(election);
+    document.getElementById("jugador").innerHTML= document.getElementById(election).outerHTML;
+    var gana = false;
+
+    var home_election = Math.floor(Math.random() * (4 - 0) + 0);
+    if (election == "paper" && options[home_election]=="scissors") {
+        puntaje--; 
+    }else if (election == "paper" && options[home_election]=="rock") {
+        puntaje++;
+        gana = true;
+    }else if (election == "rock" && options[home_election]=="scissors") {
+        puntaje++;
+        gana = true;
+    }else if (election == "rock" && options[home_election]=="paper") {
+        puntaje--;
+    }else if (election == "scissors" && options[home_election]=="paper") {
+        puntaje++;
+        gana = true;
+    }else if (election == "scissors" && options[home_election]=="rock") {
+        puntaje--;
+    }
+    actualizar(gana, home_election);
+}
+
+function actualizar(gana, home_election) {
+    setTimeout(function(){
+        document.getElementById("casa").innerHTML = document.getElementById(options[home_election]).outerHTML;;
+
+    }, 1000);
+    if (puntaje<0) {
+        console.log("El juego termina");
+    }else{
+        document.getElementById("puntaje").innerText = puntaje;
+    }
 }
